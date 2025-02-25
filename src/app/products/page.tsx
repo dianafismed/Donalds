9bc909e05042df7d183d5ac905d5f04a1e113fd3
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2025-02-25 19:13:20 -0300

    feat: add shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return <div className="p-5">
+    <h1 >Produtos</h1>
+    <Button>Botão</Button>
+    <Input></Input>
+    </div>;
+}
+ 
+export default ProductPage;
